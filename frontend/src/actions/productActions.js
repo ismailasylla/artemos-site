b27cf30cbc242ba4dd02a2e66a1d770f24702f32
ismaila sylla <ismailasyllaa@gmail.com>
2020-11-17 15:01:15 +0400

    using  Redux State to render the product lists using useDispatch & useSelector

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,4 +1,4 @@
-import axios from './axios'
+import axios from 'axios'
 import {
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Container, Row, Col } from 'react-bootstrap'
 import Product from './Product'
+import { listProducts } from '../../actions/productActions'
 import FeaturedFooter from '../FeaturedFooter'
-import axios from 'axios'
 
 const Products = () => {
-	const [products, setProducts] = useState([])
+	const dispatch = useDispatch()
+
+	const productList = useSelector((state) => state.productList)
+	const { loading, error, products } = productList
 
 	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get('/api/products')
-			setProducts(data)
-		}
-		fetchProducts()
-	}, [])
+		dispatch(listProducts())
+	}, [dispatch])
+
 	return (
 		<div>
 			<Container>
@@ -28,16 +29,22 @@ const Products = () => {
 					</Col>
 				</Row>
 			</Container>
+			{loading ? (
+				<h1 className='text-center py-3 '>Loading...</h1>
+			) : error ? (
+				<h3 className='text-center py-3 '>{error}</h3>
+			) : (
+				<Container>
+					<Row>
+						{products.map((product) => (
+							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+								<Product product={product} />
+							</Col>
+						))}
+					</Row>
+				</Container>
+			)}
 
-			<Container>
-				<Row>
-					{products.map((product) => (
-						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-							<Product product={product} />
-						</Col>
-					))}
-				</Row>
-			</Container>
 			<FeaturedFooter />
 		</div>
 	)
