ab366025f26d59f632dd2a9e42f68086fd25af33
ismaila sylla <ismailasyllaa@gmail.com>
2020-11-20 18:25:16 +0400

    Added \Custom Authentication Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,4 +29,23 @@ const authUser = asyncHandler(async (req, res) => {
 	})
 })
 
-export { authUser }
+// @desc    Get user profile
+// @route   Get /api/user/prile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id)
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		})
+	} else {
+		res.status(404)
+		throw new Error('User Not Found')
+	}
+})
+
+export { authUser, getUserProfile }
