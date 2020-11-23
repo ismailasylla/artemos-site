ce81b43ead7bc189829e2fe1f0b88901e0807a94
ismaila sylla <ismailasyllaa@gmail.com>
2020-11-23 18:45:26 +0400

    adding checkout steps to the shipping method

--- frontend/src/components/screens/ShippingScreen.jsx
+++ frontend/src/components/screens/ShippingScreen.jsx
@@ -3,6 +3,7 @@ import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../FormContainer'
 import { saveShippingAddress } from '../../actions/cartActions'
+import CheckoutSteps from '../CheckoutSteps'
 
 const ShippingScreen = ({ history }) => {
 	const cart = useSelector((state) => state.cart)
@@ -22,6 +23,7 @@ const ShippingScreen = ({ history }) => {
 	}
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2 />
 			<h1> Shipping</h1>
 			<Form onSubmit={submitHandler}>
 				<Form.Group controlId='address'>
