fb98407e961961bcfd4ba4e3e1fd5f5b5302b094
ismaila sylla <ismailasyllaa@gmail.com>
2020-11-29 01:04:02 +0400

    added PayPal Payments

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -6,6 +6,10 @@ import {
 	ORDER_DETAILS_REQUEST,
 	ORDER_DETAILS_SUCCESS,
 	ORDER_DETAILS_FAIL,
+	ORDER_PAY_REQUEST,
+	ORDER_PAY_FAIL,
+	ORDER_PAY_SUCCESS,
+	ORDER_PAY_RESET,
 } from '../constants/orderConstant'
 import { logout } from './userActions'
 
@@ -74,6 +78,51 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
 		}
 		dispatch({
 			type: ORDER_DETAILS_FAIL,
+			payload: message,
+		})
+	}
+}
+
+export const payOrder = (orderId, paymentResult) => async (
+	dispatch,
+	getState,
+) => {
+	try {
+		dispatch({
+			type: ORDER_PAY_REQUEST,
+		})
+
+		const {
+			userLogin: { userInfo },
+		} = getState()
+
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		}
+
+		const { data } = await axios.put(
+			`/api/orders/${orderId}/pay`,
+			paymentResult,
+			config,
+		)
+
+		dispatch({
+			type: ORDER_PAY_SUCCESS,
+			payload: data,
+		})
+	} catch (error) {
+		const message =
+			error.response && error.response.data.message
+				? error.response.data.message
+				: error.message
+		if (message === 'Not authorized, token failed') {
+			dispatch(logout())
+		}
+		dispatch({
+			type: ORDER_PAY_FAIL,
 			payload: message,
 		})
 	}
--- frontend/src/components/screens/OrderScreen.jsx
+++ frontend/src/components/screens/OrderScreen.jsx
@@ -1,19 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { PayPalButton } from 'react-paypal-button-v2'
 import { Link } from 'react-router-dom'
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../Message'
 import Loader from '../Loader'
-import { getOrderDetails } from '../../actions/orderActions'
-// import { ORDER_CREATE_RESET } from '../../constants/orderConstant'
+import { getOrderDetails, payOrder } from '../../actions/orderActions'
+import {
+	ORDER_PAY_RESET,
+	// ORDER_DELIVER_RESET,
+} from '../../constants/orderConstant'
 // import { USER_DETAILS_RESET } from '../../constants/userConstants'
 
 const OrderScreen = ({ match }) => {
 	const orderId = match.params.id
+
+	const [sdkReady, setSdkReady] = useState(false)
+
 	const dispatch = useDispatch()
 
 	const orderDetails = useSelector((state) => state.orderDetails)
 	const { order, loading, error } = orderDetails
+
+	const orderPay = useSelector((state) => state.orderPay)
+	const { loading: loadingPay, success: successPay } = orderPay
 
 	if (!loading) {
 		//   Calculate prices
@@ -27,8 +38,35 @@ const OrderScreen = ({ match }) => {
 	}
 
 	useEffect(() => {
-		dispatch(getOrderDetails(orderId))
-	}, [dispatch, orderId])
+		const addPayPalScript = async () => {
+			const { data: clientId } = await axios.get('/api/config/paypal')
+			const script = document.createElement('script')
+			script.type = 'text/javascript'
+			script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+			script.async = true
+			script.onload = () => {
+				setSdkReady(true)
+			}
+			document.body.appendChild(script)
+		}
+
+		if (!order || successPay || order._id !== orderId) {
+			dispatch({ type: ORDER_PAY_RESET })
+			// dispatch({ type: ORDER_DELIVER_RESET })
+			dispatch(getOrderDetails(orderId))
+		} else if (!order.isPaid) {
+			if (!window.paypal) {
+				addPayPalScript()
+			} else {
+				setSdkReady(true)
+			}
+		}
+	}, [dispatch, orderId, successPay, order])
+
+	const successPaymentHandler = (paymentResult) => {
+		console.log(paymentResult)
+		dispatch(payOrder(orderId, paymentResult))
+	}
 
 	return loading ? (
 		<Loader />
@@ -143,7 +181,19 @@ const OrderScreen = ({ match }) => {
 									</Row>
 								</ListGroup.Item>
 
-								<ListGroup.Item>{/* button paypap */}</ListGroup.Item>
+								{!order.isPaid && (
+									<ListGroup.Item>
+										{loadingPay && <Loader />}
+										{!sdkReady ? (
+											<Loader />
+										) : (
+											<PayPalButton
+												amount={order.totalPrice}
+												onSuccess={successPaymentHandler}
+											/>
+										)}
+									</ListGroup.Item>
+								)}
 							</ListGroup>
 						</Card>
 					</Col>
